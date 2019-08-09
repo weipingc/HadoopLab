97427d9e2aa8e60466b26c559b3b8c582204064c
Weiping Chen <weiping.chen@gmail.com>
2019-08-09 07:33:53 -0500

    Add tryStandalone

--- tryStandalone/runit.sh
+++ tryStandalone/runit.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "HADOOP_HOME=$HADOOP_HOME"
+# /usr/local/Cellar/hadoop/3.1.2/libexec
+
 cp -a $HADOOP_HOME/etc/hadoop/*.xml input
 rm -Rf output
 hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.1.2.jar grep input output 'h([a-z.]+\.[a-z.]+)+'
